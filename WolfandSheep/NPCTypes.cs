c6bb304be377a1a4d7a103b690936b03833aded9
andre-pedro <andrewgoesgaming@gmail.com>
2020-04-13 18:30:49 +0100

    Add NPCTypes Enum

---
+++ WolfandSheep/NPCTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WolfandSheep
+{
+    /// <summary>
+    /// Tipos de NPCs
+    /// </summary>
+    enum NPCTypes
+    {
+        Wolf,
+        Sheep
+    }
+}
--- WolfandSheep/Program.cs
+++ WolfandSheep/Program.cs
@@ -5,10 +5,30 @@ namespace WolfandSheep
 {
     class Program
     {
+        /// <summary>
+        /// Define a largura do tabuleiro
+        /// </summary>
         private const byte width = 8;
+
+        /// <summary>
+        /// Define a altura do tabuleiro
+        /// </summary>
         private const byte height = 8;
 
+        /// <summary>
+        /// Array bidimensional que cria um tabuleiro com largura e altura
+        /// </summary>
         private static byte[,] board = new byte[width, height];
+
+        /// <summary>
+        /// Instancia posição do player em X (horizontal)
+        /// </summary>
+        private static byte playerPosX = 3;
+
+        /// <summary>
+        /// Instancia posição do player em Y (vertical)
+        /// </summary>
+        private static byte playerPosY = 0;
 
         static void Main(string[] args)
         {
@@ -20,8 +40,10 @@ namespace WolfandSheep
                 }
 
                 Console.WriteLine();
-            } 
+            }
 
         }
+
+
     }
 }
