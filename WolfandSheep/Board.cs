74acd69a596c8aa29a3ff64151b1c3c1cd75da4f
Inês Barradas <inesmartinsbarradas1996@gmail.com>
2020-04-13 18:38:01 +0100

    create board class

---
+++ WolfandSheep/Board.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WolfandSheep
+{
+    public class Board
+    {
+        /// <summary>
+        /// Define a largura do tabuleiro
+        /// </summary>
+        private const byte width = 8;
+
+        /// <summary>
+        /// Define a altura do tabuleiro
+        /// </summary>
+        private const byte height = 8;
+
+        public void Render()
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write("0 ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- WolfandSheep/Program.cs
+++ WolfandSheep/Program.cs
@@ -5,42 +5,10 @@ namespace WolfandSheep
 {
     class Program
     {
-        /// <summary>
-        /// Define a largura do tabuleiro
-        /// </summary>
-        private const byte width = 8;
-
-        /// <summary>
-        /// Define a altura do tabuleiro
-        /// </summary>
-        private const byte height = 8;
-
-        /// <summary>
-        /// Array bidimensional que cria um tabuleiro com largura e altura
-        /// </summary>
-        private static byte[,] board = new byte[width, height];
-
-        /// <summary>
-        /// Instancia posição do player em X (horizontal)
-        /// </summary>
-        private static byte playerPosX = 3;
-
-        /// <summary>
-        /// Instancia posição do player em Y (vertical)
-        /// </summary>
-        private static byte playerPosY = 0;
-
         static void Main(string[] args)
         {
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    Console.Write("0 ");
-                }
-
-                Console.WriteLine();
-            }
+            Board b = new Board();
+            b.Render();
 
         }
 
