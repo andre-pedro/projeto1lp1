3b893081b4354cb4927661cfea842a1fd0517195
Inês Barradas <inesmartinsbarradas1996@gmail.com>
2020-04-13 17:42:39 +0100

    create board

--- WolfandSheep/Program.cs
+++ WolfandSheep/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace WolfandSheep
 {
     class Program
     {
+        private const byte width = 8;
+        private const byte height = 8;
+
+        private static byte[,] board = new byte[width, height];
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write("0 ");
+                }
+
+                Console.WriteLine();
+            } 
+
         }
     }
 }
